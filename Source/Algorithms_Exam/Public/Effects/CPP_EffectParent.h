7b82e85457a6b90cc1f4d754f140cdc65ab5d335
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2024-11-06 16:23:49 +0100

    Added Card Effects

---
+++ Source/Algorithms_Exam/Public/Effects/CPP_EffectParent.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPP_EffectParent.generated.h"
+
+UCLASS()
+class ALGORITHMS_EXAM_API ACPP_EffectParent : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACPP_EffectParent();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
